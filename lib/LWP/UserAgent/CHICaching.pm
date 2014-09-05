1ba15ab82bb23c863eb9211865b72fb81c5cb9dc
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-09-05 10:44:06 +0200

    some support methods

--- lib/LWP/UserAgent/CHICaching.pm
+++ lib/LWP/UserAgent/CHICaching.pm
@@ -4,6 +4,12 @@ use warnings;
 
 package LWP::UserAgent::CHICaching;
 
+extends 'LWP::UserAgent';
+use CHI;
+use Moo;
+use Types::Standard qw(InstanceOf);
+use Types::URI -all;
+
 our $AUTHORITY = 'cpan:KJETILK';
 our $VERSION   = '0.001';
 
@@ -21,6 +27,30 @@ LWP::UserAgent::CHICaching - LWP::UserAgent with caching based on CHI
 
 =cut
 
+has cache => (
+				  is => 'ro',
+				  isa => InstanceOf['CHI::Driver'],
+				  required => 1,
+				 );
+
+has key => (
+				is => 'rw',
+				isa => 'Str',
+				lazy => 1,
+				builder =>_build_key
+			  );
+
+
+has request_uri => (
+						  is =>'rw',
+						  isa => Uri,
+						 );
+
+sub _build_key {
+	my $self = shift;
+	return $self->request_uri->canonical->as_string;
+}
+
 1;
 
 __END__
@@ -36,6 +66,10 @@ L<http://rt.cpan.org/Dist/Display.html?Queue=LWP-UserAgent-CHICaching>.
 =head1 AUTHOR
 
 Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
+
+=head1 ACKNOWLEDGEMENTS
+
+This module has been strongly influenced by L<LWP::UserAgent::WithCache>
 
 =head1 COPYRIGHT AND LICENCE
 
