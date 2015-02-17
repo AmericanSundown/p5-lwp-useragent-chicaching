b39a5adacdd30108276e1c966e178fe884c11c85
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-02-17 14:26:45 +0100

    bump copyright year

--- lib/LWP/UserAgent/CHICaching.pm
+++ lib/LWP/UserAgent/CHICaching.pm
@@ -120,7 +120,7 @@ It was really nice looking at the code of L<LWP::UserAgent::WithCache>, when I w
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2014 by Kjetil Kjernsmo.
+This software is copyright (c) 2015 by Kjetil Kjernsmo.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
