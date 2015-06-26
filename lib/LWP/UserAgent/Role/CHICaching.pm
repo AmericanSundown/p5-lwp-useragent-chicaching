a6e357d4bc54b40227a3c4eccf6669003805a7ec
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-06-26 14:58:49 +0200

    implement section 3 of RFC 7234

--- lib/LWP/UserAgent/Role/CHICaching.pm
+++ lib/LWP/UserAgent/Role/CHICaching.pm
@@ -5,6 +5,8 @@ use CHI;
 use Moo::Role;
 use Types::Standard qw(Str InstanceOf);
 use Types::URI -all;
+use DateTime::Format::HTTP;
+use Try::Tiny;
 
 our $AUTHORITY = 'cpan:KJETILK';
 our $VERSION   = '0.002';
@@ -61,6 +63,11 @@ but should probably be left to be used internally for now.
 
 Wrapping L<LWP::UserAgent>'s request method.
 
+=item C<< is_shared >>
+
+A boolean value to set whether the cache can be shared. The default is
+that it is.
+
 =back
 
 =cut
@@ -86,6 +93,11 @@ has request_uri => (
 						  coerce => 1,
 						  trigger => sub { shift->clear_key },
 						 );
+
+has is_shared => (
+					is => 'rw',
+					isa => Bool,
+					default => 1);
 
 around request => sub {
 	my ($orig, $self) = (shift, shift);
@@ -115,40 +127,58 @@ around request => sub {
 		##    cacheable, and
 		# TODO: Ok, only GET supported, see above
 		
-		## o  the "no-store" cache directive (see Section 5.2) does not appear
-		##    in request or response header fields, and
 		
-		## o  the "private" response directive (see Section 5.2.2.6) does not
-		##    appear in the response, if the cache is shared, and
-		
-		## o  the Authorization header field (see Section 4.2 of [RFC7235]) does
-		##    not appear in the request, if the cache is shared, unless the
-		##    response explicitly allows it (see Section 3.2), and
-		
-		## o  the response either:
-		
-		##    *  contains an Expires header field (see Section 5.3), or
-		
-		##    *  contains a max-age response directive (see Section 5.2.2.8), or
-		
-		##    *  contains a s-maxage response directive (see Section 5.2.2.9)
-		##       and the cache is shared, or
-		
-		##    *  contains a Cache Control Extension (see Section 5.2.3) that
-		##       allows it to be cached, or
-		
-		##    *  has a status code that is defined as cacheable by default (see
-		##       Section 4.2.2), or
-		
-		##    *  contains a public response directive (see Section 5.2.2.5).
-
 		my $expires_in = 0;
 		my $res = $self->$orig(@args);
 		## o  the response status code is understood by the cache, and
 		if ($res->is_success) { # TODO: Cache only successful responses for now
+			try {
+				my $then = DateTime::Format::HTTP->parse_datetime($res->header('Expires'));
+				my $dur = $then->subtract_datetime_absolute(DateTime->now);
+				$expires_in = $dur->seconds;
+			}; # If it croaks, we will not use anyway.
 			my $cc = $res->header('Cache-Control');
 			if (defined($cc)) {
+				## o  the "no-store" cache directive (see Section 5.2) does not appear
+				##    in request or response header fields, and
+				return $res if ($cc =~ m/no-store|no-cache/); # TODO: Improve no-cache use
+				if ($self-is_shared) {
+					## o  the "private" response directive (see Section 5.2.2.6) does not
+					##    appear in the response, if the cache is shared, and
+					return $res if ($cc =~ m/private/);
+					## o  the Authorization header field (see Section 4.2 of [RFC7235]) does
+					##    not appear in the request, if the cache is shared, unless the
+					##    response explicitly allows it (see Section 3.2), and
+					if $request->header('Authorization') {
+						return $res unless ($cc =~ m/public|must-revalidate|s-maxage/);
+					}
+				}
+				## o  the response either:
+				##
+				##    *  contains an Expires header field (see Section 5.3), or
+				# Done above, so that the Expires header may be
+				# independent of the Cache-Control header
+
+				##    *  contains a max-age response directive (see Section 5.2.2.8), or
 				($expires_in) = ($cc =~ m/max-age=(\d+)/);
+
+				##    *  contains a s-maxage response directive (see Section 5.2.2.9)
+				##       and the cache is shared, or
+				if ($self->is_shared) {
+					($expires_in) = ($cc =~ m/s-maxage=(\d+)/);
+				}
+
+				##    *  contains a Cache Control Extension (see Section 5.2.3) that
+				##       allows it to be cached, or
+				# TODO
+
+				##    *  has a status code that is defined as cacheable by default (see
+				##       Section 4.2.2), or
+				# TODO: We only do GET
+
+				##    *  contains a public response directive (see Section 5.2.2.5).
+				# We do not specifically deal with this
+
 			}
 			if ($expires_in > 0) {
 				$self->cache->set($self->key, $res, { expires_in => $expires_in });
